c27d4fb3d69aec6ec7e6545acac7fe74c7be5897
Everjust <company@everjust.org>
2025-08-06 17:25:52 -0500

    Fix Auth0 integration: downgrade to v3, add API routes, remove middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import { auth0 } from '@/lib/auth0'
+import { getSession } from '@auth0/nextjs-auth0'
 import Link from 'next/link'
 
 export default async function HomePage() {
-  const session = await auth0.getSession()
+  const session = await getSession()
   const user = session?.user
 
   return (
@@ -21,6 +21,7 @@ export default async function HomePage() {
               >
                 Open Canvas
               </Link>
+              {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
               <a 
                 href="/api/auth/logout" 
                 className="inline-block bg-zinc-800 hover:bg-zinc-700 text-white px-6 py-3 rounded-lg transition-colors"
@@ -33,12 +34,14 @@ export default async function HomePage() {
           <div className="space-y-4">
             <p className="text-zinc-400">Please sign in to access your Business Model Canvas</p>
             <div className="space-x-4">
+              {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
               <a 
                 href="/api/auth/login" 
                 className="inline-block bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-lg transition-colors"
               >
                 Login
               </a>
+              {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
               <a 
                 href="/api/auth/login?screen_hint=signup" 
                 className="inline-block bg-zinc-800 hover:bg-zinc-700 text-white px-6 py-3 rounded-lg transition-colors"
